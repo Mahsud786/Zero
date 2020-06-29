166a3a91bff1922ce8edd6ac0a58e9bbf2a573ab
Mahsud786 <66312289+Mahsud786@users.noreply.github.com>
2020-06-29 23:57:19 +0500

    Update and rename zeroboy.py to Mahsud.py

--- Mahsud.py
+++ Mahsud.py
@@ -49,18 +49,13 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
+|__(~)    •||•THE - Nisar -OFFICAL------•◈•
+|__\~~) •||•Nisar - Mahsud---------------•◈•
 |__(-----\  •◈•------BLACK-TIGER--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
-\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
-\033[1;91m======================================="""
+
 
 def tik():
 	titik = ['.   ','..  ','... ']
@@ -68,27 +63,9 @@ def tik():
 		print("\r\033[1;96m[●] \x1b[1;93mSedang masuk \x1b[1;97m"+o),;sys.stdout.flush();time.sleep(1)
 
 
-back = 0
-berhasil = []
-cekpoint = []
-oks = []
-id = []
-listgrup = []
-vulnot = "\033[31mNot Vuln"
-vuln = "\033[32mVuln"
 
-os.system("clear")
-print "\033[1;96m ============================================================="
-print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
-\033[1;91m======================================="""
-print " \x1b[1;93m============================================================="
-
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "nisar"
+CorrectPassword = "nisar"
 
 loop = 'true'
 while (loop == 'true'):
